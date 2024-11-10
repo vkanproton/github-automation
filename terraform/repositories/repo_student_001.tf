f9d125a05292d1bf312950e5ad2222952cb957d7
Veselin Kanchev <vk@users.noreply.github.com>
2024-11-10 11:48:07 +0200

    vkan-test-repo

--- terraform/repositories/repo_student_001.tf
+++ terraform/repositories/repo_student_001.tf
@@ -1,3 +1,6 @@
 module "github-repository" {
-  source = "../modules/github-repository"
+  source       = "../modules/github-repository"
+  github_owner = "vkanproton"
+
+  repo_name = "vkan-test-repo"
 }
